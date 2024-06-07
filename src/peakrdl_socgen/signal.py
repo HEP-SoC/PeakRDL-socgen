7d7a30f467e2dcae9344671cb8482bff76ac69d4
Benoit Denkinger <benoit.denkinger@cern.ch>
2024-06-07 14:34:02 +0200

    SoC pkg (fixes #2)

--- src/peakrdl_socgen/signal.py
+++ src/peakrdl_socgen/signal.py
@@ -1,35 +1,24 @@
-from enum import Enum
+from typing import TYPE_CHECKING
 from systemrdl.node import SignalNode
 
-class SignalType(Enum):  # TODO make it same type as the RDL one
-    OUTPUT = 0
-    INPUT  = 1
-    BIDIRECTIONAL = 2
-    TRISTATE = 3
-    CLK = 4
-    RST = 5
-    WIRE = 6
-    BLANK = 7
-
+if TYPE_CHECKING:
+    from .intf import IntfPort
 
 class Signal:
-    def __init__(self,
-            node : SignalNode,
-            prefix : str = "",
-            cap : bool = False,
-            ):
+    """Wrapper around a SignalNode with extended properties for verilog module generation."""
+    def __init__(self, node: SignalNode, prefix: str = "", cap: bool = False):
 
         self.node = node
-
-        # self.sig_name = prefix + node.inst_name # TOOD check if correct
+        # Prefix appended to the signal name?
         self.prefix = prefix
+        # Name of the node containing the signal
         self.basename = node.inst_name
         self.cap = cap
 
         self.width = node.width
         self.is_clk = self.isClk()
         self.is_rst = self.isRst()
-        
+
         self.activelow = node.get_property('activelow', default=False) != False
         self.activehigh = node.get_property('activehigh', default=False) != False
 
@@ -41,12 +30,14 @@ class Signal:
 
     @property
     def name(self):
+        """Gets the signal instance name."""
         if self.cap:
             return self.prefix + self.node.inst_name.upper()
         return self.prefix + self.node.inst_name
 
     @property
     def verilogDir(self):
+        """Gets the signal direction in verilog format."""
         if self.input:
             return f"input {self.data_type}"
         elif self.output:
@@ -56,65 +47,61 @@ class Signal:
 
         assert False, f"Signal does not have input or output"
 
-    def isShared(self):
-        return not self.ss and not self.miso  # TODO what happens for bidir?
-
-    def isOnlyMiso(self):
-        return self.miso and not self.mosi
-
-    def isOnlyMosi(self):
-        return self.mosi and not self.miso
-
     def isClk(self):
-        typ = self.node.get_property("signal_type", default=False)
-        if typ == False:
-            return False
-        if typ.name == 'clk':
+        """Returns True if signal type is defined as clk."""
+        if self.node.get_property("clock") is not None:
             return True
-        return False
+        else:
+            return False
 
     def isRst(self):
-        typ = self.node.get_property("signal_type", default=False)
-        if typ == False:
-            return False
-        if typ.name == 'rst':
+        """Returns True if signal type is defined as rst."""
+        if self.node.get_property("reset_signal") is not None:
             return True
-        return False
+        else:
+            return False
 
     def print(self):
-        print("Signal: ", self.name, " Width: ", self.width, " Cap: ", self.cap, " Active low/high", self.activelow, self.activehigh)
+        """Prints the signal properties."""
+        print(self.__str__)
 
     def __str__(self) -> str:
-        return f"Signal: {self.name} Width: {self.width} Cap: {self.cap} Active low/high: {self.activelow} {self.activehigh}"
+        return f"Signal: {self.name} Width: {self.width} Cap: {self.cap} Active low/high: {self.activelow}/{self.activehigh}"
 
 class IntfSignal(Signal):
-
-    def __init__(self,
-            node : SignalNode,
-                 intf : "Intf", # type: ignore
-            ):
+    """Extension of the base Signal class for interface signals."""
+    def __init__(self, node: SignalNode, intf: 'IntfPort'):
 
         self.intf = intf
-
-        super().__init__(
-                node=node,
-                prefix=intf.prefix,
-                cap=intf.cap,
-                )
-
+        # Sse base class init method
+        super().__init__(node=node, prefix=intf.prefix, cap=intf.cap)
+        # Get the signal interface specific properties
         self.ss =  node.get_property('ss', default=False)    != False
         self.miso = node.get_property('miso', default=False) != False
         self.mosi = node.get_property('mosi', default=False) != False
         self.bidir = self.miso and self.mosi
 
-    def print(self):
-        print("Signal: ", self.name, " Width: ", self.width, " SS: ", self.ss, " MOSI: ", self.mosi, " MISO ", self.miso, " Cap: ", self.cap, " Active low/high", self.activelow, self.activehigh)
-
     @property
     def verilogDir(self):
+        """Gets the signal interface direction in verilog format."""
         if self.miso ^ (self.intf.modport.name == "slave"):
             return "input wire "
         elif self.mosi ^ (self.intf.modport.name == "slave"):
             return "output wire "
 
         assert False, f"Intf Signal does not have mosi or miso property"
+
+    def isShared(self):
+        """Returns True if signal is shared (e.g., an address provided to slaves?)."""
+        return not self.ss and not self.miso  # TODO what happens for bidir?
+
+    def isOnlyMiso(self):
+        """Returns True if signal is miso only."""
+        return self.miso and not self.mosi
+
+    def isOnlyMosi(self):
+        """Returns True if signal is mosi only."""
+        return self.mosi and not self.miso
+
+    def __str__(self) -> str:
+        return f"Signal: {self.name} Width: {self.width} SS: {self.ss} MOSI: {self.mosi} MISO: {self.miso} Cap: {self.cap} Active low/high: {self.activelow}/{self.activehigh}"
