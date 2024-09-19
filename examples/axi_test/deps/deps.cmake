71a93ec11e3e920a45ea463906c73e9bd6102963
Marco Andorno <marco.andorno@cern.ch>
2024-09-19 22:41:19 +0200

    Updated examples

--- examples/axi_test/deps/deps.cmake
+++ examples/axi_test/deps/deps.cmake
@@ -2,13 +2,13 @@ set(CPM_DOWNLOAD_VERSION 0.38.1)
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
 CPMAddPackage(
-    NAME SoCMake  
-    GIT_TAG v0.2.6
-    GIT_REPOSITORY "ssh://git@gitlab.cern.ch:7999/socmake/SoCMake.git"
-    )
+    NAME SoCMake
+    GIT_TAG v0.2.11
+    GIT_REPOSITORY "git@github.com:HEP-SoC/SoCMake.git"
+)
 
 CPMAddPackage(
     NAME socgen_interconnects
-    GIT_TAG master
-    GIT_REPOSITORY "ssh://git@gitlab.cern.ch:7999/socmake/socgen_interconnects.git"
-    )
+    GIT_TAG v0.1.5
+    GIT_REPOSITORY "ssh://git@gitlab.cern.ch:7999/socrates/ip_blocks/socgen_interconnects.git"
+)
